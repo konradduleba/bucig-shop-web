62c1d5e84cb47b85da4bab4fdfb8448bfacd2056
Konrad Dulęba <konrad.duleba@miquido.com>
2023-10-15 16:18:25 +0200

    feat(d): colors

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,11 +1,20 @@
-export default function RootLayout({
-  children,
-}: {
-  children: React.ReactNode;
-}) {
+import { FC, PropsWithChildren } from 'react';
+import { Inter } from 'next/font/google';
+
+import '../theme/_theme.scss';
+
+const inter = Inter({
+  weight: ['400', '500', '600', '700'],
+  display: 'swap',
+  subsets: ['latin'],
+});
+
+const RootLayout: FC<PropsWithChildren> = ({ children }) => {
   return (
-    <html lang="en">
+    <html lang="en" className={inter.className}>
       <body>{children}</body>
     </html>
   );
-}
+};
+
+export default RootLayout;
