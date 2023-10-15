ac2b98497096ec3e4dbf7f34e3fc2f61098b979d
Konrad Dulęba <konrad.duleba@miquido.com>
2023-10-15 22:04:58 +0200

    feat(general): add head static (temporary) metadata

--- src/app/about-us/page.tsx
+++ src/app/about-us/page.tsx
@@ -1,4 +1,10 @@
+import { Metadata } from 'next';
 import { FC } from 'react';
+
+export const metadata: Metadata = {
+  title: 'Bucig - About us',
+  description: 'Bucig - Description about us',
+};
 
 const Page: FC = () => {
   return (
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,17 @@
-export default function Page() {
+import { Metadata } from 'next';
+import { FC } from 'react';
+
+export const metadata: Metadata = {
+  title: 'Bucig - Łokulary',
+  description: 'Bucig - Opis Łokularów',
+};
+
+const Page: FC = () => {
   return (
     <div>
-      <h1>Hello, Next.js! Updated</h1>
-      <p>Prettier test</p>
+      <h1>Hello, Next.js!</h1>
     </div>
   );
-}
+};
+
+export default Page;
