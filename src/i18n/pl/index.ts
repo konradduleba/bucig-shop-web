307236dc8efa4d2fd023cdf29e67686e7a2d1aef
jedrzej.knapik@miquido.com <jedrzej.knapik@b-connected.io>
2023-10-29 20:35:45 +0100

    fix(general): add export to pl translations

---
+++ src/i18n/pl/index.ts
@@ -0,0 +1,8 @@
+import type { BaseTranslation } from '../i18n-types';
+
+const pl = {
+  // TODO: your translations go here
+  HI: 'Cześć {name:string}!',
+} satisfies BaseTranslation;
+
+export default pl;
