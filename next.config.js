1005ac4e67fb6b2c1d413ef68afad6ab2cbd0929
jedrzej.knapik@miquido.com <jedrzej.knapik@b-connected.io>
2023-11-14 09:34:29 +0100

    feat(general): add default language redirect

--- next.config.js
+++ next.config.js
@@ -1,4 +1,14 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  redirects: async () => {
+    return [
+      {
+        source: '/',
+        destination: `/pl`,
+        permanent: true,
+      },
+    ];
+  },
+};
 
 module.exports = nextConfig;
