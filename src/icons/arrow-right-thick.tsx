61d908e67d79a24eca04939e9848d31d274d523a
Jędrzej Knapik <69152019+jedrzejknapik@users.noreply.github.com>
2023-12-10 16:44:04 +0100

    feat(general): add main content rwd (#17)
    Co-authored-by: jedrzej.knapik@miquido.com <jedrzej.knapik@b-connected.io>

---
+++ src/icons/arrow-right-thick.tsx
@@ -0,0 +1,38 @@
+import type { SVGProps } from 'react';
+import { memo } from 'react';
+const wrapperStyles = {
+  transformOrigin: 'center center',
+};
+const Svg = (props: SVGProps<SVGSVGElement>): JSX.Element => (
+  <svg
+    xmlns="http://www.w3.org/2000/svg"
+    fill="none"
+    viewBox="0 0 24 24"
+    role="img"
+    width="1em"
+    height="1em"
+    {...props}
+  >
+    <path
+      stroke="currentColor"
+      strokeLinecap="round"
+      strokeLinejoin="round"
+      strokeWidth={1}
+      d="M3 12h18M16 7l5 5-5 5"
+    />
+  </svg>
+);
+const SvgArrowRightThick = memo((props: SVGProps<SVGSVGElement>) => (
+  <Svg
+    {...props}
+    style={{
+      ...wrapperStyles,
+      ...(props.style || {}),
+    }}
+    role="img"
+    width="1em"
+    height="1em"
+  />
+));
+SvgArrowRightThick.displayName = 'SvgArrowRight';
+export default SvgArrowRightThick;
